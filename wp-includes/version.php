c4ae166534a168c42dd1028c200a8915428d6131
wildworks <wildworks@git.wordpress.org>
2026-09-08 10:58:50 +0000

    Docs: Align `@param` tag columns in Style Engine docblocks.
    Aligns `@param` tag columns in docblocks across the Style Engine per the PHP inline documentation standards.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13415
    
    Props mukesh27, wildworks.
    See #65818.
    
    Built from https://develop.svn.wordpress.org/trunk@63528
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62704 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63527';
+$wp_version = '7.2-alpha-63528';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
